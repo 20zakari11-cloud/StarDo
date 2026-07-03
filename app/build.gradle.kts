a5f120f2a35b47906051489bfd7db3d93c7216b1
20zakari11-cloud <20zakari11@gmail.com>
2026-07-03 15:06:17 +0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,3 +1,8 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
 android {
     namespace = "com.stardo"
     compileSdk = 34
@@ -10,14 +15,6 @@ android {
         versionName = "1.0"
     }
 
-    buildFeatures {
-        compose = true
-    }
-
-    composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.8"
-    }
-
     buildTypes {
         release {
             isMinifyEnabled = false
@@ -27,13 +24,4 @@ android {
 
 dependencies {
     implementation("androidx.core:core-ktx:1.12.0")
-    implementation("androidx.activity:activity-compose:1.8.2")
-
-    implementation(platform("androidx.compose:compose-bom:2023.10.01"))
-
-    implementation("androidx.compose.ui:ui")
-    implementation("androidx.compose.material3:material3")
-    implementation("androidx.compose.ui:ui-tooling-preview")
-
-    debugImplementation("androidx.compose.ui:ui-tooling")
 }
