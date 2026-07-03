c9a1cbfb00eba1497d6f4eb52be69bbc1b4ddc8f
20zakari11-cloud <20zakari11@gmail.com>
2026-07-03 13:57:21 +0300

    Create build.gradle.kts

---
+++ app/build.gradle.kts
@@ -0,0 +1,27 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "com.stardo"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.stardo"
+        minSdk = 24
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+        }
+    }
+}
+
+dependencies {
+    implementation("androidx.core:core-ktx:1.12.0")
+}
