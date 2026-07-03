dabe9f673a9d0551375943ddbaee6de47833bf12
20zakari11-cloud <20zakari11@gmail.com>
2026-07-03 15:38:55 +0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,8 +37,13 @@ android {
 dependencies {
     implementation("androidx.core:core-ktx:1.12.0")
 
-    // Compose
-    implementation(platform("androidx.compose:compose-bom:2024.06.00"))
     implementation("androidx.activity:activity-compose:1.8.2")
+
+    implementation(platform("androidx.compose:compose-bom:2023.10.01"))
+
+    implementation("androidx.compose.ui:ui")
+    implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
+
+    debugImplementation("androidx.compose.ui:ui-tooling")
 }
